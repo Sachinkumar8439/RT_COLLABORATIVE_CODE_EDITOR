9e1b397e01b7edc4b63e1096b53b9511e3c5d16d
sachin kumar <sachin39430@gmail.com>
2025-04-03 23:12:35 +0530

    new feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StateProvider } from "./Context/usecontext";
 import { SocketProvider } from "./Context/SocketContetx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Maineditor from "./components/Maineditor";
-import LiveEditor from "./components/LiveEditor"
+import LiveEditor from "./components/LiveEditor";
 import "./App.css";
 import AuthForm from "./components/AuthForm ";
 import Check from "./components/Check";
 
 function App() {
+  
+
   return (
     <SocketProvider>
-
-    <StateProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/ghgf" element={<Check />} />
-            <Route path="/editor/:id" element={<Maineditor />} />
-            <Route path="/Authorization" element={<AuthForm/>} />
-            <Route path="/edit/p/:userid/:programid" element={<LiveEditor/>} />
-          </Routes>
-        </div>
-      </Router>
-    </StateProvider>
+      <StateProvider>
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={ <Homepage />} />
+              <Route path="/ghgf" element={<Check />} />
+              <Route path="/editor/:id" element={<Maineditor />}/>
+              <Route path="/Authorization" element={<AuthForm/>} />
+              <Route path="/edit/p/:userid/:programid" element={<LiveEditor />} />
+            </Routes>
+          </div>
+        </Router>
+      </StateProvider>
     </SocketProvider>
   );
 }
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Hero from './Hero';
+import { useNavigate } from 'react-router-dom';
+
 
 const Homepage = () => {
+ 
+
+
 
 
   return (
