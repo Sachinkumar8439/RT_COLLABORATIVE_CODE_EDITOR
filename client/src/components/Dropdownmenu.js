49c06c31f4ec148c671b02bcc69f1ea74c09a912
sachin kumar <sachin39430@gmail.com>
2025-04-04 19:00:18 +0530

    list dikh rhi h click bhi ho rha hai

--- client/src/components/Dropdownmenu.js
+++ client/src/components/Dropdownmenu.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import programs from "../Controllers/program";
+import { monacoFormatLang } from "../data";
 
-const DropdownMenu = ({ isVisible, position, onClose,language,handleOpenPopup }) => {
+const DropdownMenu = ({ isVisible, position, onClose,language,handleOpenPopup,files,currentfile,setcurrentfile,fileref}) => {
   const menuRef = useRef();
-
+ 
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (menuRef.current && !menuRef.current.contains(event.target)) {
@@ -23,14 +24,6 @@ const DropdownMenu = ({ isVisible, position, onClose,language,handleOpenPopup })
   }, [isVisible]);
 
   if (isVisible === false) return null;
-  let listItems = [];
-  for (let i = 0; i < 5; i++) {
-    listItems.push(
-      <li key={i} style={{ padding: "2px 4px", cursor: "pointer" }}>
-        {i}
-      </li>
-    );
-  }
 
   return (
     
@@ -39,20 +32,21 @@ const DropdownMenu = ({ isVisible, position, onClose,language,handleOpenPopup })
       style={{
         position: "absolute",
         top: position.y,
-        left: position.x-80,
-        background: "black",
+        left: position.x-90,
+        background: "rgb(255, 255, 255)",
         border: "1px solid #ccc",
         borderRadius: "5px",
         boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
         listStyle: "none",
-        padding: "3px",
-        margin: 0,
+        padding: "5px",
+        // margin: 0,
         zIndex: 1000,
-        width: "",
-        color:'white',
+        width: "180px",
+        maxHeight:'400px'
+        // color:'white',
       }}
       >
-        <input type='text' placeholder="find file"></input>
+        {/* <input style={{padding:'5px',borderRadius:'5px',border:'2px solid green'}} type='text' placeholder="find file"></input> */}
       <li
         className="menu-item"
         style={{
@@ -60,6 +54,7 @@ const DropdownMenu = ({ isVisible, position, onClose,language,handleOpenPopup })
           padding: "2px 4px",
           cursor: "pointer",
           whiteSpace: "nowrap",
+          margin:'5px'
         }}
         onClick={(e) => {
           e.preventDefault();
@@ -67,10 +62,49 @@ const DropdownMenu = ({ isVisible, position, onClose,language,handleOpenPopup })
           onClose();
         }}
       >
-        new file
+      + new file
       </li>
+      {files && files.map((file, index) => {
+        const match = monacoFormatLang.find(v => v.extension === file.extension);
+        const filematch = file._id === (currentfile && currentfile._id);
+      return (
+        <li
+        // ref={fileref}
+        data-thisfile = {JSON.stringify(file)}
+        key={file._id}
+        className="menu-item"
+        
+        style={{
+          // background:'grey',
+          padding: "5px 4px",
+          cursor: "pointer",
+          whiteSpace: "nowrap",
+          color:filematch ? "white":'',
+          background:filematch?'rgb(7, 61, 87)':"",
+        }}
+        onClick={(e) => {
+          e.preventDefault();
+          fileref.current = e.currentTarget; 
+          const fileDataString = e.currentTarget.dataset.thisfile;
+          const fileData = JSON.parse(fileDataString);
+          console.log('Clicked element:', fileref.current);
+          console.log('File data:', file);
+          onClose();
+          setcurrentfile(fileData);
+          localStorage.setItem('lastfile',JSON.stringify(fileData))
+        }}
+      >
+        <div style={{display:'flex',gap:'10px'}}>
 
-      {listItems}
+        <img src={match.iconUrl} width='20px' alt=''/>
+       <p>
+        {`${file.fileName}.${file.extension}`}
+       </p>
+        </div>
+      </li>
+  );
+})}
+
     </ul>
   );
 };
